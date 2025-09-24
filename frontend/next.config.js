c1848d74c8fbedda34ebcee700081d4f4f11e9f5
Muhammad Muzammil Tahir <mztahir1234@gmail.com>
2025-09-24 17:35:45 +0500

    updated url

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: { domains: ['localhost'] },
+  images: { domains: ['localhost', 'chatbot.dipietroassociates.com'] },
 };
 module.exports = nextConfig;
